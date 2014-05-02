758683389bc8f556d112ffa3ea4d477f9c9724f5
Tomaz Bracic <tomaz.bracic@gmail.com>
2014-05-02 22:35:05 +0200

    added images, fixtures, blog entity, show page, etc

---
+++ src/Tomaz/BlogElastikaBundle/Controller/BlogController.php
@@ -0,0 +1,32 @@
+<?php
+
+// src/Tomaz/BlogElastikaBundle/Controller/BlogController.php
+
+namespace Tomaz\BlogElastikaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+/**
+ * Blog Controller
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $blog = $em->getRepository('TomazBlogElastikaBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find blog post');
+        }
+
+        return $this->render('TomazBlogElastikaBundle:Blog:show.html.twig', array(
+            'blog' => $blog,
+        ));
+    }
+}
